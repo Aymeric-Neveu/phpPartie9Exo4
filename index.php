869c7f38fd35ef3e9ab85dc163e54fb5ee3cb67b
root <emploi.neveu@gmail.com>
2018-12-04 11:58:14 +0100

    exo4

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 4</title>
+  </head>
+  <body>
+     <p><?php echo 'Le timeStamp actuel est ' . time();?></p>
+     <p><?php echo 'Le timeStamp du mardi 2 août 2016 est ' . mktime(15, 0, 0, 8, 2, 2016); ?>
+  </body>
+</html>
